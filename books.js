a17bdce8d7c584a3e2c617bf455d2450b0c40ee5
yishai <yishai5555@gmail.com>
2016-08-10 11:53:35 +0300

    b2

--- books.js
+++ books.js
@@ -1,48 +1,53 @@
 
 var books = [
     {
+        id:1,
         title: 'War and Peace',
         genre: 'Historical Fiction',
         author: 'Lev Nikolayevich Tolstoy',
         read: false
     },
     {
-        title: 'Les Misérables',
+        id:2,title: 'Les Misérables',
         genre: 'Historical Fiction',
         author: 'Victor Hugo',
         read: false
     },
     {
-        title: 'The Time Machine',
+        id:3,title: 'The Time Machine',
         genre: 'Science Fiction',
         author: 'H. G. Wells',
         read: false
     },
     {
-        title: 'A Journey into the Center of the Earth',
+        id:4,title: 'A Journey into the Center of the Earth',
         genre: 'Science Fiction',
         author: 'Jules Verne',
         read: false
     },
     {
+        id:5,
         title: 'The Dark World',
         genre: 'Fantasy',
         author: 'Henry Kuttner',
         read: false
     },
     {
+        id:6,
         title: 'The Wind in the Willows',
         genre: 'Fantasy',
         author: 'Kenneth Grahame',
         read: false
     },
     {
+        id:7,
         title: 'Life On The Mississippi',
         genre: 'History',
         author: 'Mark Twain',
         read: false
     },
     {
+        id:8,
         title: 'Childhood',
         genre: 'Biography',
         author: 'Lev Nikolayevich Tolstoy',
--- booksRoutes.js
+++ booksRoutes.js
@@ -23,38 +23,136 @@ bookRouter.route('/Books')
         // });
     })
 
-   .post(function (req, res) {
+    .post(function (req, res) {
 
         // var book = new Book(req.body);
         //book.save();
 
         var book = req.body;
+        book.id = getNextId(books);
+
         books.push(book);
 
-        res. status(201)
+        res.status(201)
             .json(books);
     });
 
 
-bookRouter.route('/Books/:bookId')
-    .get(function (req, res) {
+bookRouter.route('/Books/:bookId').get(function (req, res) {
 
-        var title = req.params.bookId;
-        console.log(title);
-        var selectedBook = books.filter(function (item) {
-            return item.title == title;
-        });
+    var id = req.params.bookId;
 
-        res.json(selectedBook);
+    console.log(id);
 
-        /*
-         Book.findById(req.params.bookId, function(err,book){
-         if(err)
-         res.status(500).send(err);
-         else
-         res.json(book);
-         });*/
+    var selectedBook = books.filter(function (item) {
+        return item.id == id;
     });
 
+    if (selectedBook.length) {
+        selectedBook = selectedBook[0];
+    }
+    else {
+        selectedBook = {};
+    }
+
+    res.json(selectedBook);
+
+    /*
+     Book.findById(req.params.bookId, function(err,book){
+     if(err)
+     res.status(500).send(err);
+     else
+     res.json(book);
+     });*/
+});
+
+bookRouter.route('/Books/:bookId').put(function (req, res) {
+
+    var book = req.body;
+    var id = req.params.bookId;
+
+
+    var selectedBook = books.filter(function (item) {
+        return item.id == id;
+    });
+
+    if (selectedBook.length) {
+        selectedBook = selectedBook[0];
+    }
+    else {
+        selectedBook = null;
+    }
+
+    if (selectedBook) {
+        selectedBook.title = book.title;
+        selectedBook.genre = book.genre;
+        selectedBook.author = book.author;
+        selectedBook.read = book.read;
+
+        res.status(204)
+            .json(books);
+    }
+    else {
+
+        res.status(404)
+            .json(books);
+    }
+});
+
+bookRouter.route('/Books/:bookId').delete(function (req, res) {
+    var id = req.params.bookId;
+
+    var selectedBook = books.filter(function (item) {
+        return item.id == id;
+    });
+
+    if (selectedBook.length) {
+        selectedBook = selectedBook[0];
+    }
+    else {
+        selectedBook = null;
+    }
+
+    if (selectedBook) {
+        books.splice(books.indexOf(selectedBook),1);
+    }
+
+    res.status(204)
+        .json(books);
+});
+
+function getNextId(data) {
+
+    var nextId = getMaxId(data) + 1;
+    console.log('next ', nextId)
+
+    return nextId;
+
+    // return getMaxId(data) + 1;
+}
+
+function getMaxId_not_good(books) {
+    if (books && books.length > 0) {
+        return books[books.length - 1].id;
+    }
+    return 0;
+}
+
+
+function getMaxId(books) {
+    if (books && books.length > 0) {
+        var idArray = books.map(function (item) {
+            return item.id;
+        });
+
+        return maxArrayValue(idArray);
+    }
+
+    return 0;
+}
+
+function maxArrayValue(array) {
+    return Math.max.apply(Math, array);
+}
 
 module.exports = bookRouter;
